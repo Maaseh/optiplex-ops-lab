e79879368620734b211f20d6cb677865f741d961
Thomas Bonnet <bnt.thomas@icloud.com>
2025-08-19 22:40:54 +0200

    overall file update

---
+++ scripts/template_vm_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#####################################
+#
+# Author: Thomas "maaseh" Bonnet
+# Description: This script lays the foundation 
+# for new VMs enabling homogeneous systems.
+#
+#####################################
+
+# Variables
+
+
